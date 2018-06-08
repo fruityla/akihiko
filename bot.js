86d9ba229ffe4018ac174a8a78f2bda6ccb448bf
fruityla <40044621+fruityla@users.noreply.github.com>
2018-06-08 08:44:03 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,4 +11,16 @@ bot.on('message', message => {
     }
 });
     
+bot.on("guildMemberAdd", function(member){
+    var membername = member.toString();
+    var sentences = new Array(10);
+    var random = getRandomInt(0,4); // lay random so (so' toi thieu, so' toi da)
+    sentences[0] = "It's about time you got here, "+membername;
+    sentences[1] = membername+", welcome home.";
+    sentences[2] = membername+", where have you been?";
+    sentences[3] = membername+", nice timing!";
+    sentences[4] = membername+", let's have a great match!";
+    member.guild.channels.find("name","tartarus").sendMessage( sentences[random]);
+});
+
 bot.login(process.env.BOT_TOKEN);
