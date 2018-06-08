a7686bb1d9470e527371df2e7b7ab6073ac2ccf9
fruityla <40044621+fruityla@users.noreply.github.com>
2018-06-08 08:51:07 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,20 +7,22 @@ bot.on('ready', () => {
 
 bot.on('message', message => {
     if (message.content === 'akihiko') {
-        message.reply('you okay?');
+        message.reply('I love you');
     }
 });
     
-bot.on("guildMemberAdd", function(member){
-    var membername = member.toString();
-    var sentences = new Array(10);
-    var random = getRandomInt(0,4);
-    sentences[0] = "It's about time you got here, "+membername;
-    sentences[1] = membername+", welcome home.";
-    sentences[2] = membername+", where have you been?";
-    sentences[3] = membername+", nice timing!";
-    sentences[4] = membername+", let's have a great match!";
-    member.guild.channels.find("name","tartarus").sendMessage( sentences[random]);
-});
+if (msgCase.includes("say fuck")) {
+var mentions = new Array(10);
+var random = getRandomInt(0,7); 
+mentions[0] = "... F-fuck?";
+mentions[1] = "H-Hamuko!";
+mentions[2] = "FUCK!";
+mentions[3] = "Don't swear!";
+mentions[4] = "Do you ever stop?";
+mentions[5] = "fucckkk got dammmmmmmmmmmit sonnnn of a bitchhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh ahhhhhhhhhhhhhhhhhhhhhhhhhhhh I spilled my baja blast";
+mentions[6] = "Stop bossing me around!"
+mentions[7] = "Tch! Fuck off!";
+message.channel.sendMessage(mentions[random]);
+};
 
 bot.login(process.env.BOT_TOKEN);
