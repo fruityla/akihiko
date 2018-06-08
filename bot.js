d2e1a9e40c7bb484b5cb6517ad67c65e172b5139
fruityla <40044621+fruityla@users.noreply.github.com>
2018-06-08 08:36:31 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,14 @@
-aaaaa
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'akihiko') {
+        message.reply('Ready!');
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
